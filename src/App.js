efbfe35334d0df362abfc1e31b90a0e9ab7a5c11
SohanWasker <waskersohan@gmail.com>
2023-03-28 17:01:09 +0530

    updated App File

--- src/App.js
+++ src/App.js
@@ -53,6 +53,77 @@ function App() {
       <p>Turns: {turns} </p>
     </div>
   );
+<<<<<<< Updated upstream
+=======
+    const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)  
+      .map((card) => ({ ...card, id: Math.random()}))
+      
+      setChoiceOne(null)
+      setChoiceTwo(null)
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+  
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+  
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else{
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      } 
+    }
+  },[choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card =>(
+          <SingleCard 
+          key={card.id} 
+          card={card} 
+          handleChoice={handleChoice} 
+          flipped={card === choiceOne || card === choiceTwo || card.matched} 
+          disabled={disabled}
+          />
+         ))}
+      </div>
+      <p>Turns: {turns} </p>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App
